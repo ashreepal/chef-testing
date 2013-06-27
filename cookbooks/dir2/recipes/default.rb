c27155a68ad16247a94d1ac4b1dd2c696393cd8a
Shreepal Shah <ubuntu@ip-10-154-175-91.ec2.internal>
2013-06-27 18:01:15 +0000

    extra test to make sure conditionals held up even with run_action

--- cookbooks/dir2/recipes/default.rb
+++ cookbooks/dir2/recipes/default.rb
@@ -1,12 +1,14 @@
 include_recipe 'dir1::default'
 
-directory "/home/ubuntu/dir1/dir2" do
+dir2_file = directory "/home/ubuntu/dir1/dir2" do
   owner "ubuntu"
   group "ubuntu"
   mode '0755'
-  action :create
+  action :nothing
 
   only_if do
     ::File.exists?("/home/ubuntu/dir1")
   end
 end
+
+dir2_file.run_action(:create)
