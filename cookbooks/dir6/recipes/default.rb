853ba9be2fd911e6a463b2a0c9cab4c849603088
Shreepal Shah <ubuntu@ip-10-154-175-91.ec2.internal>
2013-06-27 17:19:03 +0000

    Successfully tested conditional statements like only_if to control when events occur

--- cookbooks/dir6/recipes/default.rb
+++ cookbooks/dir6/recipes/default.rb
@@ -4,23 +4,27 @@ dir8_file = directory "/home/ubuntu/dir1/dir2/dir3/dir4/dir5/dir6/dir7/dir8" do
   owner "ubuntu"
   group "ubuntu"
   mode '0755'
-  action :nothing
+  action :create
+
+  only_if do
+    ::File.exists?("home/ubuntu/dir1/dir2/dir3/dir4/dir5/dir6/dir7")
+  end
 end
 
 dir7_file = directory "/home/ubuntu/dir1/dir2/dir3/dir4/dir5/dir6/dir7" do
   owner "ubuntu"
   group "ubuntu"
   mode '0755'
-  action :nothing
+  action :create
+  only_if do
+    ::File.exists?("home/ubuntu/dir1/dir2/dir3/dir4/dir5/dir6")
+  end
+
 end
 
 dir6_file = directory "/home/ubuntu/dir1/dir2/dir3/dir4/dir5/dir6" do
   owner "ubuntu"
   group "ubuntu"
   mode '0755'
-  action :nothing
+  action :create
 end
-
-dir6_file.run_action(:create)
-dir7_file.run_action(:create)
-dir8_file.run_action(:create)
